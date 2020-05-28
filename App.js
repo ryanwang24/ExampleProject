1304d98efa61b0bc3169f56128f5af92a5c2022c
Ryan Wang <ryanwang@Ryans-MacBook-Pro-4.local>
2020-05-28 12:41:27 -0700

    Class 6 Fancy Box

--- App.js
+++ App.js
@@ -9,7 +9,7 @@ import {
 } from "react-native";
 import GoalItem from "./components/GoalItem";
 import Box from "./components/Box";
-import GoalInput from "./components/GoalInput"
+import GoalInput from "./components/GoalInput";
 function doSomething() {
   var x = 0;
   console.log("x is equal to: " + x);
@@ -17,41 +17,36 @@ function doSomething() {
   console.log("x is equal to: " + x);
 }
 
+import Header from "./components/Header";
+
 export default function App() {
   const [courseGoals, setCourseGoals] = useState([]);
 
   const addGoalHandler = (goalTitle) => {
     setCourseGoals((currentGoals) => [
       ...currentGoals,
-       {id: Math.random().toString(), value: goalTitle}
-      ]);
+      { id: Math.random().toString(), value: goalTitle },
+    ]);
   };
 
   return (
-    <View style={styles.root}>
-      <GoalInput addGoalHandler={addGoalHandler}/>
-      <FlatList
-        keyExtractor={(item, index) => item.id}
-        data={courseGoals}
-        renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
-      />
-      {/* <Box/> */}
+    <View>
+      <Box />
+      <View style={styles.root}>
+        <GoalInput addGoalHandler={addGoalHandler} />
+        <FlatList
+          keyExtractor={(item, index) => item.id}
+          data={courseGoals}
+          renderItem={(itemData) => <GoalItem title={itemData.item.value} />}
+        />
+      </View>
     </View>
-    
   );
 }
 
 const styles = StyleSheet.create({
   root: {
     padding: 50,
-  },
-  box: {
-    width: 1000,
-    height: 100,
-    backgroundColor: "lightblue",
-    borderColor: "black",
-    borderWidth: 1,
-    padding: 10,
   },
   boxContainer: {
     alignItems: "center",
--- components/Box.js
+++ components/Box.js
@@ -1,15 +1,33 @@
-import React from'react'
-import {View,StyleSheet} from 'react-native'
+import React from "react";
+import { View, StyleSheet, Text } from "react-native";
 
-export default Box = () =>{return <View style={styles.box}/> }
-
+export default Box = () => {
+  return (
+    <View style={styles.box}>
+      <Text style={styles.text}>Enter grades</Text>
+    </View>
+  );
+};
 const styles = StyleSheet.create({
-box: {
-    width: 1000,
-    height: 100,
-    backgroundColor: "lightblue",
-    borderColor: "black",
-    borderWidth: 1,
+  box: {
+    width: "100%",
+    height: "25%",
+    backgroundColor: "rgba(154,129,255,0.5)",
+    justifyContent: "flex-end",
+    alignItems: "center",
     padding: 10,
-  }
+    shadowColor: "#000",
+    shadowOffset: {
+      width: 0,
+      height: 2,
+    },
+    shadowOpacity: 0.23,
+    shadowRadius: 2.62,
+
+    elevation: 4,
+  },
+  text: {
+    color: "#6646e6",
+    fontSize: 30,
+  },
 });
